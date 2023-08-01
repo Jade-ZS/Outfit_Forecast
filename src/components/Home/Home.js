c8977a82cc4f4c4585aca9c29157bec8c5b7bd6f
Jade Shi <ziyushi1997@gmail.com>
2023-07-31 23:24:06 -0700

    Add prop method to add weather

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,24 @@
 import './Home.css';
 import SearchBar from '../SearchBar/SearchBar';
 import ResultContainer from '../ResultContainer/ResultContainer';
+import { useState } from 'react';
 
 export default function Home() {
+  const [isValid, setIsValid] = useState(true);
+  const [result, setResult] = useState({});
+
+  const addWeather = weather => {
+    setResult({...result, weather: weather});
+  }
+
+  const checkAddress = value => {
+    setIsValid(value);
+  }
+
   return(
     <div className='home-page'>
-      <SearchBar />
-      <ResultContainer />
+      <SearchBar addWeather={addWeather} checkAddress={checkAddress}/>
+      <ResultContainer isValid={isValid} />
     </div>
   );
 }
--- src/components/WeatherCard/WeatherCard.js
+++ src/components/WeatherCard/WeatherCard.js
@@ -3,7 +3,11 @@ import './WeatherCard.css';
 function WeatherCard() {
   return (
     <div className='weather-card'>
-      
+      <div className='city-name'>
+        <span></span>
+        <sup></sup>
+
+      </div>
     </div>
   )
 }
