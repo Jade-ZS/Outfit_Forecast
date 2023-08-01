20a7c5bb5b05dffab120f01c1d9d78af13be756a
Jade Shi <ziyushi1997@gmail.com>
2023-08-01 00:13:16 -0700

    Style WeatherCard

--- src/components/WeatherCard/WeatherCard.js
+++ src/components/WeatherCard/WeatherCard.js
@@ -5,8 +5,9 @@ export default function WeatherCard({result}) {
     <div className='weather-card'>
       {console.log('weathercard result: ', result)}
       <div className='city-name'>
-        <span>{JSON.stringify(result)}</span>
-        <sup></sup>
+        {/* <span>{JSON.stringify(result)}</span> */}
+        <span>{result.name}</span>
+        <sup>{result.sys.country}</sup>
       </div>
     </div>
   )
