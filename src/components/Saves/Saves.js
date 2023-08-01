2277cfb68f91dd6d7fa837f99b6c006b397c15a8
Jade Shi <ziyushi1997@gmail.com>
2023-08-01 11:22:27 -0700

    Add link to home button

--- src/components/Saves/Saves.js
+++ src/components/Saves/Saves.js
@@ -1,9 +1,10 @@
 import './Saves.css';
+import { Link } from 'react-router-dom';
 
 export default function Saves() {
   return (
     <div className='saves'>
-      {console.log('here saves')}
+      <Link to='/'><p>🏠</p></Link>
       <p>saved</p>
       <h1>let us save</h1>
     </div>
