df9fc00de0a327330d2e57de8fa727570cea7b44
Jade Shi <ziyushi1997@gmail.com>
2023-08-01 19:40:42 -0700

    Add SaveContext

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,29 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, createContext } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from '../Home/Home';
 import NotFound from '../NotFound/NotFound';
 import Saves from '../Saves/Saves';
 import SingleView from '../SingleView/SingleView';
+import { SaveContext } from '../../SaveContext';
 
 function App() {
+
   const [saves, setSaves] = useState([]);
-  const saveLocation = location => {
+  const addSave = location => {
     setSaves([...saves, location]);
   }
 
   return (
     <div>
-      <Routes>
-        <Route path='/' element={<Home saveLocation={saveLocation}/>} />
-        <Route path='/saved' element={<Saves /> } />
-          {/* <Route path=':id' element={<SingleView />} /> */}
-        {/* <Route path='*' element={<NotFound />} /> */}
-      </Routes>
+      <SaveContext.Provider value={{saves, setSaves, addSave}}>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/saved' element={<Saves /> } />
+            {/* <Route path=':id' element={<SingleView />} /> */}
+          {/* <Route path='*' element={<NotFound />} /> */}
+        </Routes>
+      </SaveContext.Provider>
     </div>
   );
 }
--- src/components/Saves/Saves.js
+++ src/components/Saves/Saves.js
@@ -1,7 +1,11 @@
 import './Saves.css';
 import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { SaveContext } from '../../SaveContext';
 
 export default function Saves() {
+  const { saves, setSaves } = useContext(SaveContext)
+  console.log('saves Saves: ', saves)
   return (
     <div className='saves'>
       <Link to='/'><p>🏠</p></Link>
