59bb9aac12622b5c0fe9433daf40f5fea8cb5e02
Jade Shi <ziyushi1997@gmail.com>
2023-08-02 09:41:21 -0700

    Add dynamic save toggle

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -1,29 +1,41 @@
 import './Result.css';
 import WeatherCard from '../WeatherCard/WeatherCard';
 import { SaveContext } from '../../SaveContext';
-import { useState, useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
 
 export default function Result({result}) {
   const { saves, addSave, deleteSave } = useContext(SaveContext);
-  const [isSaved, setIsSaved] = useState(true);
+  const [isSaved, setIsSaved] = useState(false);
+  const location = result && result.coord;
 
+  const checkIfSaved = () => {
+    const output = saves.some(element => JSON.stringify(element) === JSON.stringify(location));
+    console.log('checkIfSaved output: ', output, 'isSaved: ', isSaved)
+    return output;
+  }
+
+  useEffect(() => {
+    setIsSaved(checkIfSaved());
+  }, [saves, location])
+
+  const handleSaveClick = () => {
+    if(isSaved) {
+      deleteSave(location);
+    } else {
+      addSave(location);
+    }
+    console.log('onclick saves: ', saves)
+  }
 
   return (
     <div>
       {result && 
       <>
        <img 
-          className={`save-button ${!isSaved && 'clicked'}`}
+          className={`save-button ${isSaved && 'clicked'}`}
           src='https://img.icons8.com/?size=512&id=82461&format=png'
           alt='save button'
-          onClick = {() => {
-            if(isSaved) {
-              deleteSave(result.coord);
-            } else {
-              addSave(result.coord);
-            }
-            console.log('onclick saves: ', saves)
-          }}
+          onClick = {handleSaveClick}
         />
 
         <div className='result-display'>
