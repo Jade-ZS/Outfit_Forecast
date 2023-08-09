5cd97ca25b4e339aafb93dc11b235e3c662289b7
Jade Shi <ziyushi1997@gmail.com>
2023-08-08 19:41:29 -0700

    Add temp button

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,10 +6,12 @@ import NotFound from '../NotFound/NotFound';
 import Saves from '../Saves/Saves';
 import SingleView from '../SingleView/SingleView';
 import { SaveContext } from '../../SaveContext';
+import { UnitContext } from '../../UnitContext';
 
 function App() {
   const [fetchErr, setFetchErr] = useState(false);
   const [saves, setSaves] = useState([]);
+  const [unit, setUnit] = useState('C');
   const checkErr = (value) => setFetchErr(value); 
   const addSave = location => setSaves([...saves, location]);
   const deleteSave = location => {
@@ -20,15 +22,19 @@ function App() {
   return (
     <div>
       <SaveContext.Provider value={{saves, addSave, deleteSave}}>
-        <Routes>
-          <Route path='/' element={<Home checkErr={checkErr}/>} />
-          <Route path='/Result' element={fetchErr ? <NotFound /> : <Home checkErr={checkErr}/>} />
-          <Route path='/saved'>
-            <Route index element={<Saves />}/>
-            <Route path=':id' element={<SingleView />} />
-          </Route>
-          <Route path='*' element={<NotFound />} />
-        </Routes>
+        <UnitContext.Provider value={{unit, setUnit}}>
+          <button className='f-button'>F</button>
+          <button className='c-button'>C</button>
+          <Routes>
+            <Route path='/' element={<Home checkErr={checkErr}/>} />
+            <Route path='/Result' element={fetchErr ? <NotFound /> : <Home checkErr={checkErr}/>} />
+            <Route path='/saved'>
+              <Route index element={<Saves />}/>
+              <Route path=':id' element={<SingleView />} />
+            </Route>
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+        </UnitContext.Provider>
       </SaveContext.Provider>
     </div>
   );
