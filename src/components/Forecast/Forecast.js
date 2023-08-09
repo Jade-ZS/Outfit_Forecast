3b2823a75bd30ba3108939891960bed3034bede4
Jade Shi <ziyushi1997@gmail.com>
2023-08-09 16:46:11 -0700

    Add unit conversion to forecast temp

--- src/components/Forecast/Forecast.js
+++ src/components/Forecast/Forecast.js
@@ -1,17 +1,20 @@
 import './Forecast.css';
 import { fetchForecast } from '../../apiCalls';
-import { useState } from 'react';
+import { useContext } from 'react';
 import { Accordion, AccordionItemHeading, AccordionItem, AccordionItemButton, AccordionItemPanel } from 'react-accessible-accordion';
-
+import { UnitContext } from '../../UnitContext';
 
 export default function Forecast({forecast}) {
+  const { unit, CtoF } = useContext(UnitContext);
   const weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
   return (
     <div className='forecast-container'>
       <p>forecast section</p>
       <label>Daily</label>
       <Accordion allowZeroExpanded>
-        {forecast.list.slice(0, 7).map((item, index) => (
+        {forecast.list.slice(0, 7).map((item, index) => {
+          let min_temp = Math.round(item.main.temp_min);
+          return (
           <AccordionItem key={index}>
             <AccordionItemHeading>
               <AccordionItemButton>
@@ -22,13 +25,14 @@ export default function Forecast({forecast}) {
                   />
                   <label className='day'>{weekdays[index]}</label>
                   <label classname='description'>{item.weather[0].description}</label>
-                  <label className='min-max'>{Math.round(item.main.temp_min)}&deg;C</label>
+                  <label className='min-max'>{unit === 'C' ? min_temp : CtoF(min_temp)}&deg;{unit}</label>
                 </div>
               </AccordionItemButton>
             </AccordionItemHeading>
             <AccordionItemPanel></AccordionItemPanel>
           </AccordionItem>
-        ))}
+          );
+        })}
       </Accordion>
     </div>
   )
