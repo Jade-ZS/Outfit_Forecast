a73aa432dc1b779129d0d5d38099d3dd802f16ef
Jade Shi <ziyushi1997@gmail.com>
2023-08-08 23:39:23 -0700

    Add Forecast state

--- src/components/Forecast/Forecast.js
+++ src/components/Forecast/Forecast.js
@@ -1,6 +1,10 @@
 import './Forecast.css';
+import { fetchForecast } from '../../apiCalls';
+import { useState } from 'react';
 
 export default function Forecast() {
+  const [forecasat, setforecast] = useState({});
+  
   return (
     <div className='forecast-container'>
     </div>
