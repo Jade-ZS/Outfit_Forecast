092bbe5b739cf6300eb799c52f09334d50750c74
Jade Shi <ziyushi1997@gmail.com>
2023-08-10 15:01:33 -0700

    Add get current location

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -18,6 +18,18 @@ function App() {
     const updatedSaves = saves.filter(element => JSON.stringify(element) !== JSON.stringify(location))
     setSaves(updatedSaves);
   };
+  const [currentLat, setCurrentLat] = useState('');
+  const [currentLng, setCurrentLng] = useState('');
+  const getCurrentLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setCurrentLat(position.coords.latitude);
+        setCurrentLng(position.coords.longitude);
+      }, e => {
+        console.log(e)
+      })
+    }
+  }
  
 
   return (
@@ -26,6 +38,9 @@ function App() {
         <UnitContext.Provider value={{unit, CtoF}}>
           <button className={`f-button ${unit === 'F' && 'clicked'}`} onClick={() => setUnit('F')}>&deg;F</button>
           <button className={`c-button ${unit === 'C' && 'clicked'}`} onClick={() => setUnit('C')}>&deg;C</button>
+          <button onClick={getCurrentLocation}>location</button>
+          <p>lat: {currentLat}</p>
+          <p>lng: {currentLng}</p>
             <Routes>
               <Route path='/' element={<Home checkErr={setFetchErr} />} />
               <Route path='/Result' element={fetchErr ? <NotFound /> : <Home checkErr={setFetchErr} />} />
